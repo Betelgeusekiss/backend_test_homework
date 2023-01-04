c34b323269ac8bbd80deadc6e3c13a7b9cc60e23
Комаров Олег <oleg16-90@mail.ru>
2023-01-04 23:53:00 +0300

    posts

---
+++ hw02_community/yatube/posts/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Post, Group
+
+NUM_OF_PUBLICATIONS = 10
+
+
+def index(request):
+    posts = Post.objects.all()[:NUM_OF_PUBLICATIONS]
+    context = {
+        'posts': posts,
+    }
+    return render(request, 'posts/index.html', context)
+
+
+def group_list(request, slug):
+    group = get_object_or_404(Group, slug=slug)
+    posts = Post.objects.filter(group=group)[:NUM_OF_PUBLICATIONS]
+    context = {
+        'text': slug,
+        'group': group,
+        'posts': posts,
+    }
+    return render(request, 'posts/group_list.html', context)
